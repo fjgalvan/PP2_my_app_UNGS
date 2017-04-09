f893e12660c2a9e540dd1d87eeb1927623bcfd70
fjgalvan <fjgalvanx87@gmail.com>
2017-04-09 16:13:06 -0300

    se reinsertaron los sonidos

--- my-app/src/main/java/sonido/Sonido.java
+++ my-app/src/main/java/sonido/Sonido.java
@@ -7,9 +7,9 @@ import java.applet.AudioClip;
 public class Sonido {
 	public static final AudioClip TanqueDisparo = Applet
 			.newAudioClip(Sonido.class.getResource
-					("/Tanque Disparo.wav"));//("/sonido/Tanque Disparo.wav"));
+					("/sonido/Tanque Disparo.wav"));
 	
 	public static final AudioClip TanqueDisparoExplocion = Applet
 			.newAudioClip(Sonido.class.getResource
-					("/Tanque Disparo Explocion.wav"));//("/sonido/Tanque Disparo Explocion.wav"));
+					("/sonido/Tanque Disparo Explocion.wav"));
 }
